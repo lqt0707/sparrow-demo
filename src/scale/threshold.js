ff6fb0f864a6279aba4cf7680402b0e53c5034d3
qiutao.liu <qiutao.liu@7x-network.com>
2022-08-10 23:12:58 +0800

    feat:complete renderer and scale

--- src/scale/threshold.js
+++ src/scale/threshold.js
@@ -1,5 +1,5 @@
 export function createThreshold({ domain, range }) {
-  const n = Math.main(domain.length, range.length - 1);
+  const n = Math.min(domain.length, range.length - 1);
   return (x) => {
     const index = domain.findIndex((v) => x < v);
     return range[index === -1 ? n : index];
