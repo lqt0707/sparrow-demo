f4d288a0ad93b991d0b146d7a8ea676fd24bca25
qiutao.liu <qiutao.liu@7x-network.com>
2022-08-10 22:43:26 +0800

    feat:test scale

--- src/scale/linear.js
+++ src/scale/linear.js
@@ -1,5 +1,5 @@
 import { interpolateNumber } from "./interpolate";
-import { ceil, floor, normalize, ticks, tickStep } from "./utils";
+import { ceil, floor, nice, normalize, ticks, tickStep } from "./utils";
 
 export function createLinear({
   domain: [d0, d1],
@@ -11,8 +11,9 @@ export function createLinear({
     return interpolate(t, r0, r1);
   };
 
-  scale.ticks = (tickCount) => ticks(d0, d1, tickCount);
-  scale.nice = (tickCount) => {
+  scale.ticks = (tickCount=10) => ticks(d0, d1, tickCount);
+  scale.nice = (tickCount=10) => {
+    if (d0===d1) return;
     const step = tickStep(d0, d1, tickCount);
     [d0, d1] = nice([d0, d1], {
       floor: (x) => floor(x, step),
