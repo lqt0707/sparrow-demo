3cfb3d7d517f4c6a4769ecd1c937feb3574b8e88
lqt0707 <1444706897@qq.com>
2022-08-28 22:41:50 +0800

    feat(view):view develop

---
+++ src/view/utils.js
@@ -0,0 +1,16 @@
+export function descendants(root) {
+  const nodes = [];
+  const push = (d) => nodes.push(d);
+  bfs(root, push);
+  return nodes;
+}
+
+// 使用宽度优先搜索遍历这棵树
+export function bfs(root, callback) {
+  const discovered = [root];
+  while (discovered.length) {
+    const node = discovered.pop();
+    callback(node);
+    discovered.push(...(node.children || []));
+  }
+}
